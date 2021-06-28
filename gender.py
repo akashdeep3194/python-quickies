89023894d30f423ecb19ea5cc7d78de3814746a6
Akash <akashdeep3194@gmail.com>
2021-06-29 03:45:32 +0530

    Added subsequence.py and pycharm files

--- gender.py
+++ gender.py
@@ -1,13 +1,25 @@
-def gender(n,k):
-    if n==1:
-        return "M"
-    if gender(n-1 ,int((k+1)/2)) == "M":
-        if k%2 == 0:
-            return "F"
+import sys
+
+
+def gender(n, k):
+    if n == 1:
+        return "Male"
+    if gender(n - 1, int((k + 1) / 2)) == "Male":
+        if k % 2 == 0:
+            return "Female"
         else:
-            return "M"
+            return "Male"
     else:
-        if k%2 == 0:
-            return "M"
+        if k % 2 == 0:
+            return "Male"
         else:
-            return "F"
+            return "Female"
+
+
+t = int(input())
+while t > 0:
+    arr = list(map(int, sys.stdin.readline().strip().split()))
+    x = arr[0]
+    y = arr[1]
+    print(gender(x, y))
+    t -= 1
